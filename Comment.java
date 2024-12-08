7b1cd9ca4c165421e2e0e4564d79ccc04dc4b2aa
Pooja More <155895360+Pooja-More15@users.noreply.github.com>
2024-12-08 14:27:15 +0530

    Add files via upload

---
+++ Comment.java
@@ -0,0 +1,11 @@
+public class Comment {
+    public static void main(String args []){
+        //This is single line comment
+        System.out.println("Pooja More");
+        /* This is 
+         multiline 
+         comment */
+         System.out.println("I m a 23 year old");
+
+    }
+}
