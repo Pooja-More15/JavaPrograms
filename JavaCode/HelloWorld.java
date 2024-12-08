5816fb6b3755628fdb384b0ca85ef0bdb8fb25c8
Pooja More <155895360+Pooja-More15@users.noreply.github.com>
2024-12-08 14:13:47 +0530

    Add files via upload

---
+++ JavaCode/HelloWorld.java
@@ -0,0 +1,9 @@
+public class HelloWorld {
+    public static void main(String args[] )
+    {
+        //println used for nextLine
+        System.out.println("Hello World");
+        //using print....it does not generate nextLine
+        System.out.print("Hello Worlds");
+    }
+}
